5890ad4d198d20a546c880bf0b4fe69894dad68f
Ranjan <rkranjan.rk33@gmail.com>
2022-07-16 22:33:18 +0545

    change

---
+++ vanilla.js
@@ -0,0 +1,72 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+
+
+//{let value='';
+// let n=0;
+// function display(num1){
+    // if(value.length>9){
+        // alert("Maximum 10 digits allowed");
+    // }
+    // else{
+        // n=n+1;
+        // value=value+num1;
+        // document.querySelector(".button").innerHTML=value;}
+//}
+
+// document.querySelector(".1").addEventListener("click",()=>{
+    // list(1);
+//})
+// document.querySelector(".2").addEventListener("click",()=>{
+    // list(2);
+// })
+//document.querySelector(".3").addEventListener("click",()=>{
+ //   list(3);
+//})
+//document.querySelector(".4").addEventListener("click",()=>{
+  //  list(4);
+//})
+//document.querySelector(".5").addEventListener("click",()=>{
+  //  list(5);
+//})
+//document.querySelector(".6").addEventListener("click",()=>{
+  //  list(6);
+//})
+
+//document.querySelector(".7").addEventListener("click",()=>{
+  //  list(7);
+//})
+//document.querySelector(".8").addEventListener("click",()=>{
+  //  list(8);
+//})
+//document.querySelector(".9").addEventListener("click",()=>{
+  //  list(9);
+//})
+//document.querySelector(".*").addEventListener("click",()=>{
+ //   list("*");
+//})
+//document.querySelector(".0").addEventListener("click",()=>{
+  //  list(0);
+//})
+//document.querySelector(".#").addEventListener("click",()=>{
+  //  list("#");
+//})
+//document.querySelector(".back").addEventListener("click",()=>{
+//    if(value1.length<=0){
+//        alert("No digits left");
+//    }
+  //  else{
+  //      n=n-1;
+    //    value1=value1.substr(0,value1.length-1);
+      //  document.querySelector(".dis").innerHTML=value1;
+    //}
+//})
+
